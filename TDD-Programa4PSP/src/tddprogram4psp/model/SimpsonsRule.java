04363f079554479c47137d8c4afdf87f63907010
estudiantelis <estudiantelis@lis-sala2>
2016-10-06 18:05:52 -0500

    Comentario secuencias taller 5

--- TDD-Programa4PSP/src/tddprogram4psp/model/SimpsonsRule.java
+++ TDD-Programa4PSP/src/tddprogram4psp/model/SimpsonsRule.java
@@ -34,23 +34,33 @@ public class SimpsonsRule {
     }
     
     public double calculateIteration(double x, int dof, int numSeg, IFunction function){
+        //Sentencia #1
         double w;
         double p;
+        //Sentencia #2
         if(dof <= 0){
+            //Sentencia #3
             throw new ArithmeticException("El número de grados de libertad deben ser positivo");
         }
+        //Sentencia #4
         if(numSeg <= 0 || (numSeg % 2 == 1)){
+            //Sentencia #5
             throw new ArithmeticException("El número de segmentos debe ser positivo y par");
         }
+        //Sentencia #6
         w = x / numSeg;
         p = 0;
         p = p + function.evaluate(0);
         for (int i = 1; i <= (numSeg-1); i=i+2) {
+            //Sentencia #7
             p = p + (4 * function.evaluate(i*w));
         }
+        //Sentencia #8
         for (int i = 2; i <= (numSeg-2); i=i+2) {
+            //Sentencia #9
             p = p + (2 * function.evaluate(i*w));            
         }
+        //Sentencia #10
         p = p + function.evaluate(x);
         p = p * (w/3);
         return p;
